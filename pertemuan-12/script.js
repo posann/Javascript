2527ecbeaee1a9e3dde0f1ee1ddbd186e78c6b34
Ahmad Fauzan <57349734+afzn9x@users.noreply.github.com>
2023-10-24 11:04:27 +0700

    update

--- pertemuan-12/script.js
+++ pertemuan-12/script.js
@@ -1,10 +1,34 @@
-let form = document.forms.form_coba;
-let element1 = form.elements.element_1;
-let element2 = form.elements.element_2;
-let button = form.elements.element_button;
+// let form = document.forms.form_coba;
+// let element1 = form.elements.element_1;
+// let element2 = form.elements.element_2;
+// let button = form.elements.element_button;
 
 
-button.addEventListener("click", function(){
-    let hasil = parseFloat(element1.value) + parseFloat(element2.value)
-    alert(hasil)
-})
+// button.addEventListener("click", function(){
+//     let hasil = parseFloat(element1.value) + parseFloat(element2.value)
+//     alert(hasil)
+// })
+
+let nama_mhs = [
+    'Rio',
+    'Abil',
+    'Haeril',
+    'Aryadi'
+]
+
+let asal_mhs = [
+    'Afrika',
+    'Jamaika',
+    'Israel',
+    'Gaza'
+]
+
+function pendaftaran_mhs(nama, asal){
+    console.log(nama);
+    console.log(asal);
+}
+
+for(i=0 ; i<4 ; i++){
+    pendaftaran_mhs(nama_mhs[i], asal_mhs[i])
+}
+
