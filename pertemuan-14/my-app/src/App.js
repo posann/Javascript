593ee5e190bd939f239250d3a199dd21b60c5e72
Ahmad Fauzan <57349734+afzn9x@users.noreply.github.com>
2023-10-26 11:02:58 +0700

    update

---
+++ pertemuan-14/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/home';
+import Header from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ pertemuan-14/my-app/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div>
+            <nav>
+                <h2>My App</h2>
+            </nav>
+        </div>
+    )
+}
+
+export default Home;
